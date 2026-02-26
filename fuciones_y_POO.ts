af4c9c88508ce04fb991ff95fc4efdbdc88f7e8a
miguel <miguel@miguel.gumorose.com>
2026-02-25 20:28:47 -0800

    calcular el precio final de un producto

--- fuciones_y_POO.ts
+++ fuciones_y_POO.ts
@@ -4,3 +4,9 @@ una función, colocamos los dos puntos : y el tipo justo después de
 los paréntesis de los parámetros.
 */
 //Mira esta función que calcula el precio final de nuestro Producto:
+function obtenerPrecioFinal(producto: Producto): number {
+    if (producto.descuento) {
+        return producto.precio - producto.descuento;
+    }
+    return producto.precio;
+}
