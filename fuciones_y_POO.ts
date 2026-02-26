bc3b1ad5bbd19ad13fac270b21affcaea42b1ceb
miguel <miguel@miguel.gumorose.com>
2026-02-25 20:23:19 -0800

    tipar el retorno de la funcion (lo que sale)

---
+++ fuciones_y_POO.ts
@@ -0,0 +1,6 @@
+/*
+Para decirle a TypeScript qué tipo de dato va a devolver exactamente ç
+una función, colocamos los dos puntos : y el tipo justo después de
+los paréntesis de los parámetros.
+*/
+//Mira esta función que calcula el precio final de nuestro Producto:
