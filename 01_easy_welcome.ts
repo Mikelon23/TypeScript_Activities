12d5edc4e6d8cc221dfb30130fda38a2cf430f39
Miguel <tierram928@gmail.com>
2026-03-04 20:13:28 -0800

    mapa de mensaje basado en roles con el principio de Resposabilidad Unica (SRP)

---
+++ 01_easy_welcome.ts
@@ -0,0 +1,15 @@
+/**
+ * EJERCICIO 1: Nivel Fácil - Sistema de Saludos Personalizados
+ * 
+ * OBJETIVO: 
+ * Crear una función que reciba un objeto de usuario y devuelva un mensaje de bienvenida.
+ * Se debe aplicar Clean Code mediante el uso de interfaces y tipos claros.
+ */
+
+enum UserRole {
+    ADMIN = 'ADMIN',
+    EDITOR = 'EDITOR',
+    GUEST = 'GUEST',
+}
+
+
