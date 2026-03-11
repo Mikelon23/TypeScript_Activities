81baa602408539a9ed0bae7b5212b7c7fe638eed
Miguel <tierram928@gmail.com>
2026-03-10 21:16:49 -0700

    ESquema buider y validator 'Zod' se define la estructura que se quiere z.string para validar data en tiempo de ejecucion

---
+++ 05_schema_validator.ts
@@ -0,0 +1,19 @@
+/**
+ * Ejercicio 5: TypeScript Schema Validator (Mini-Zod) 
+ * Dificultad: Alta (Requiere entender fuertemente inferencia condicional)
+ * 
+ * Problema en la vida real: 
+ * Cuando recibimos un JSON de una API externa o un formulario (express.body), 
+ * TypeScript no nos protege en tiempo de ejecución. 
+ * Con este validador, construimos esquemas que validan la data en runtime Y ADEMÁS
+ * deduce estáticamente (compile-time) la interfaz de TypeScript, matando dos pájaros de un tiro.
+ */
+
+// Extrae el tipo inferido de un Validador. ¡Magia negra de TypeScript!
+type Infer<T> = T extends Validator<infer U> ? U : never;
+
+// Clase base abstracta
+abstract class Validator<T> {
+  abstract parse(val: unknown): T;
+}
+
