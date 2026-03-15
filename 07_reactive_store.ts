8f3d91aec63b601174741e94aa197054e809e949
Miguel <tierram928@gmail.com>
2026-03-15 09:35:22 -0700

    Motor reactivo de estado central - cualquier objeto que pase sera parasitado recursivamente

---
+++ 07_reactive_store.ts
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 7: Gestor de Estado Reactivo Impulsado por Proxies
+ * Dificultad: Alta-Experta
+ * 
+ * Problema en la vida real:
+ * Libraries como MobX, Vue Reactivity, o SolidJS detectan mágicamente cuando
+ * cambias una variable profunda como state.user.name = "Otro", y repintan el HTML solos.
+ * ¿Cómo hacen para saber que tocaste una variable en JS sin setter explícito?
+ * Respuesta: Metaprogramación con el Objeto `Proxy` e interceptores de la API `Reflect`.
+ */
+
+type Listener = () => void;
+
+class ReactiveStore<T extends object> {
+  private listeners: Set<Listener> = new Set();
+  public reactiveState: T;
+
